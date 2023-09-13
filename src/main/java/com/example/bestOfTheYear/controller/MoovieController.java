d1ee9648997e52c7e626523a46d4b310c2f1b5ee
Oblv49 <noprivacyworld@gmail.com>
2023-09-13 10:25:05 +0200

    moovies and songs controller

--- src/main/java/com/example/bestOfTheYear/controller/MoovieController.java
+++ src/main/java/com/example/bestOfTheYear/controller/MoovieController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/moovies")
+@RequestMapping("/movies")
 public class MoovieController {
     @GetMapping
     public String movieList() {
-        return "moovies";
+        return "movies";
     }
 }
