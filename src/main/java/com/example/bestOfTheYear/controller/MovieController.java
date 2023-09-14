33df4df2002aa765be4554141b1c2a7073b26515
Oblv49 <noprivacyworld@gmail.com>
2023-09-14 09:07:16 +0200

    navigation movies end

--- src/main/java/com/example/bestOfTheYear/controller/MovieController.java
+++ src/main/java/com/example/bestOfTheYear/controller/MovieController.java
@@ -1,11 +1,13 @@
 package com.example.bestOfTheYear.controller;
 
 import com.example.bestOfTheYear.model.Movie;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +31,12 @@ public class MovieController {
                 movieToFind = m;
             }
         }
+
+        if (movieToFind == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Movie with id " + movieId + " not found");
+        }
+
         model.addAttribute("movieIdFind", movieToFind);
         return "movieDetail";
     }
